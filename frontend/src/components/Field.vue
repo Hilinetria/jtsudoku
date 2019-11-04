db7d24befa8f51f6efbdd19806e28276be2bde97
Artem Kabanov <oloreas@gmail.com>
2019-11-04 20:59:08 +0500

    add frontend

---
+++ frontend/src/components/Field.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="field">
+      <tr v-for="row in generated" v-bind:key="row">
+        <td class="cell" v-for="column in row" v-bind:key="column">
+          {{ column }}
+        </td>
+      </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'Field',
+  props: ['generated'],
+}
+</script>
+
+<style scoped>
+  .field {
+    border: 1px;
+    margin: 0 auto;
+  }
+  .cell {
+    padding: 5px 9px;
+    border: 1px solid black;
+  }
+</style>
